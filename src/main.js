81547978e66fd6f3f0b0a97524336d2c743cb0f6
Angelo Manfredi <manfrediangelosantiago10@gmail.com>
2024-05-08 18:13:47 -0300

    add primevue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import PrimeVue from "primevue/config";
+import App from "./App.vue";
+
+//in main.js
+import "primevue/resources/themes/aura-light-green/theme.css";
+
+const app = createApp(App);
+app.use(PrimeVue);

4ee66984d363c2291f3e7941b41f1ba39cd78608
Angelo Manfredi <manfrediangelosantiago10@gmail.com>
2024-05-08 19:27:52 -0300

    the styles of addTasladoButton are modified

--- src/components/AddTrasladoButton.vue
+++ src/components/AddTrasladoButton.vue
@@ -14,7 +14,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 button {
   background-color: #23a98d;
   border: none;
@@ -25,5 +25,9 @@ button {
   display: inline-block;
   font-size: 16px;
 }
- 
+
+button:hover {
+  background-color: #1b6f5b;
+}
+
 </style>

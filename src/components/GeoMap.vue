661e084eff0647440fe7d7f951f0cb1a71bf649f
Angelo Manfredi <manfrediangelosantiago10@gmail.com>
2024-05-17 11:15:05 -0300

    refactor del script de creacion del mapa

--- src/components/GeoMap.vue
+++ src/components/GeoMap.vue
@@ -4,54 +4,52 @@
   </div>
 </template>
 
-<script>
-import { defineComponent, onMounted, onBeforeMount } from 'vue'
+<script setup>
+import { onMounted, onBeforeMount } from 'vue'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 
-export default defineComponent({
-  name: 'LMap',
-  props: {
-    markers: {
-      type: Array,
-      required: false,
-      default: () => []
-    }
-  },
-  setup(props) {
-    let map = null
+let map = null
 
-    onMounted(() => {
-      console.log(props.markers)
-      createMapLayer()
-    })
-
-    onBeforeMount(() => {
-      if (map) {
-        map.remove()
-      }
-    })
-
-    const createMapLayer = () => {
-      map = L.map('mapContainer').setView([-26.8560346, -49.239189], 5)
-      L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map)
-
-      if (props.markers.length) {
-        setMarkers()
-      }
-    }
-
-    const setMarkers = () => {
-      props.markers.map((marker) => {
-        return L.marker([marker.latitude, marker.longitude]).addTo(map)
-          .bindPopup(marker.descricao)
-      })
-    }
+const props = defineProps({
+  markers: {
+    type: Array,
+    required: false,
+    default: () => []
   }
 })
+
+onMounted(() => {
+  console.log(props.markers)
+  createMapLayer()
+})
+
+onBeforeMount(() => {
+  if (map) {
+    map.remove()
+  }
+})
+
+const createMapLayer = () => {
+  map = L.map('mapContainer').setView([-26.8560346, -49.239189], 5)
+  L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map)
+
+  if (props.markers.length) {
+    setMarkers()
+  }
+}
+
+const setMarkers = () => {
+  props.markers.map((marker) => {
+    return L.marker([marker.latitude, marker.longitude]).addTo(map)
+      .bindPopup(marker.descricao)
+  })
+}
+
+
 </script>
 
 <style scoped>
